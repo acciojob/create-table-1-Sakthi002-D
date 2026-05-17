298b9b2037804285942ece111919c71e5c804b65
Sakthi002-D <sakthivel.d002@gmail.com>
2026-05-17 16:27:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
-  
+let table = document.getElementById("sampleTable");
+
+  // new row create
+let newRow = table.insertRow(0);
+
+  // 2 cells create
+let cell1 = newRow.insertCell(0);
+let cell2 = newRow.insertCell(1);
+
+  // values add
+cell1.innerHTML = "New Cell1";
+cell2.innerHTML = "New Cell2";
+}
   
 }
